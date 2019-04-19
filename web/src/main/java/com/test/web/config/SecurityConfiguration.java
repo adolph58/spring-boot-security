0d2469dcb14230105052344bb33cd0cdadacfb14
chengjiantao <jaunty5@163.com>
2019-04-19 10:04:39 +0800

    init

---
+++ web/src/main/java/com/test/web/config/SecurityConfiguration.java
@@ -0,0 +1,108 @@
+package com.test.web.config;
+
+import com.test.web.service.CustomUserDetailsService;
+import com.test.web.service.LoginSuccessHandler;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@EnableConfigurationProperties(SecuritySettings.class)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    protected Log log = LogFactory.getLog(getClass());
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private SecuritySettings settings;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+        //remember me
+        auth.eraseCredentials(false);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login").permitAll() //设置一个使用自定义的登录页面 URL
+                .successHandler(loginSuccessHandler()) //设置自定义的一个登录成功处理器
+                .and().authorizeRequests()
+                .antMatchers("/images/**", "/checkcode", "/scripts/**", "/styles/**").permitAll() //是完全允许访问的一些 URL 配置，并可以使用通配符来设置，这里将一些资源目录赋予可以完全访问的权限，由 settings 指定的权限列表也赋予了完全访问的权限
+                .antMatchers(settings.getPermitall().split(",")).permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().requireCsrfProtectionMatcher(csrfSecurityRequestMatcher()) //即跨站请求伪造（cross-site request forgery），这是一个防止跨站请求伪造攻击的策略设置
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and().logout() //设置使用默认的登出
+                .logoutSuccessUrl(settings.getLogoutsuccssurl()) //设定登出成功的链接
+                .and().exceptionHandling().accessDeniedPage(settings.getDeniedpage()) //配置一个拒绝访问的提示链接
+                .and().rememberMe().tokenValiditySeconds(86400).tokenRepository(tokenRepository()); //用来记住用户的登录状态，即用户没有执行退出时，再次打开页面将不用登录
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JdbcTokenRepositoryImpl tokenRepository(){
+        JdbcTokenRepositoryImpl jtr = new JdbcTokenRepositoryImpl();
+        jtr.setDataSource(dataSource);
+        return jtr;
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler(){
+        return new LoginSuccessHandler();
+    }
+
+    @Bean
+    public CustomFilterSecurityInterceptor customFilter() throws Exception{
+        CustomFilterSecurityInterceptor customFilter = new CustomFilterSecurityInterceptor();
+        customFilter.setSecurityMetadataSource(securityMetadataSource());
+        customFilter.setAccessDecisionManager(accessDecisionManager());
+        customFilter.setAuthenticationManager(authenticationManager);
+        return customFilter;
+    }
+
+    @Bean
+    public CustomAccessDecisionManager accessDecisionManager() {
+        return new CustomAccessDecisionManager();
+    }
+
+    @Bean
+    public CustomSecurityMetadataSource securityMetadataSource() {
+        return new CustomSecurityMetadataSource(settings.getUrlroles());
+    }
+
+
+    private CsrfSecurityRequestMatcher csrfSecurityRequestMatcher(){
+        CsrfSecurityRequestMatcher csrfSecurityRequestMatcher = new CsrfSecurityRequestMatcher();
+        List<String> list = new ArrayList<String>();
+        list.add("/rest/");
+        csrfSecurityRequestMatcher.setExecludeUrls(list);
+        return csrfSecurityRequestMatcher;
+    }
+}
